mwilky/smali-enchilada

OPLongshot: save screens as PNG

OPLongshot/smali/com/oneplus/screenshot/util/Utils.smali
@@ -913,7 +913,7 @@
 
     move-object v6, v14
 
-    sget-object v14, Landroid/graphics/Bitmap$CompressFormat;->JPEG:Landroid/graphics/Bitmap$CompressFormat;
+    sget-object v14, Landroid/graphics/Bitmap$CompressFormat;->PNG:Landroid/graphics/Bitmap$CompressFormat;
 
     const/16 v15, 0x64
 
@@ -975,7 +975,7 @@
 
     const-string v0, "mime_type"
 
-    const-string v2, "image/jpeg"
+    const-string v2, "image/png"
 
     invoke-virtual {v8, v0, v2}, Landroid/content/ContentValues;->put(Ljava/lang/String;Ljava/lang/String;)V
 

IrishHuman/smali-guacamole

OPLongshot/smali/com/oneplus/screenshot/util/Utils.smali
@@ -1745,7 +1745,7 @@
 
     move-object v7, v0
 
-    sget-object v0, Landroid/graphics/Bitmap$CompressFormat;->JPEG:Landroid/graphics/Bitmap$CompressFormat;
+    sget-object v0, Landroid/graphics/Bitmap$CompressFormat;->PNG:Landroid/graphics/Bitmap$CompressFormat;
 
     const/16 v2, 0x64
 
@@ -2007,7 +2007,7 @@
     :cond_4
     const-string v0, "mime_type"
 
-    const-string v5, "image/jpeg"
+    const-string v5, "image/png"
 
     invoke-virtual {v9, v0, v5}, Landroid/content/ContentValues;->put(Ljava/lang/String;Ljava/lang/String;)V
 